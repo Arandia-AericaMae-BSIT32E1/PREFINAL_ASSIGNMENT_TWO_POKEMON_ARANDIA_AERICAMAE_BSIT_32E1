a463f55d3d5544e9ef76ffdadd82af245a958591
Arandia-AericaMae-BSIT32E1 <arandiaaerica@gmail.com>
2024-05-26 00:51:40 +0800

    added in  models pokemon

---
+++ ArandiaPokemonBSIT32E1/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+namespace ArandiaPokemonBSIT32E1.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<string> Abilities { get; set; }
+        public List<string> Moves { get; set; }
+    }
+}
--- ArandiaPokemonBSIT32E1/obj/Debug/net8.0/ArandiaPokemonBSIT32E1.AssemblyInfo.cs
+++ ArandiaPokemonBSIT32E1/obj/Debug/net8.0/ArandiaPokemonBSIT32E1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArandiaPokemonBSIT32E1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bef25b046054cfecfb99d7ef21320166f8f0831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79cb137971086ec72798cfad0ddca0bb591b41cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArandiaPokemonBSIT32E1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArandiaPokemonBSIT32E1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
